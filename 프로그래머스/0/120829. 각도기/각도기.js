301f27be6be68f14928565c8e7501f95dfa6464a
임채이 <babybreadin1025@gmail.com>
2024-11-21 18:15:23 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,17 +1,4 @@
 function solution(angle) {
-    let answer = 0;
-    if (angle > 0 && 90>angle){
-        return 1;
-    }
-    else if (angle == 90){
-        return 2;
-    }
-    else if(angle > 90 && 180>angle){
-        return 3;
-    }
-    else if (angle == 180){
-        return 4;
-    }
-    
+    var answer = 0;
     return answer;
 }
